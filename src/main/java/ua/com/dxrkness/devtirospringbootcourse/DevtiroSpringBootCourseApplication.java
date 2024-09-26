c76569719652b4e037b2c6f86ae937c8cde5a53a
dxrknesss <imp0rt4ntduck@gmail.com>
2024-09-26 18:01:18 +0300

    feat: initial commit

---
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/DevtiroSpringBootCourseApplication.java
@@ -0,0 +1,13 @@
+package ua.com.dxrkness.devtirospringbootcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtiroSpringBootCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevtiroSpringBootCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/dxrkness/devtirospringbootcourse/DevtiroSpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.dxrkness.devtirospringbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtiroSpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
