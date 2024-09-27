85f98129cb835b92e69b5793e61c08083331aa4f
dxrknesss <imp0rt4ntduck@gmail.com>
2024-09-27 21:05:46 +0300

    test: add unit test for BookDAO's create method

--- src/test/java/ua/com/dxrkness/devtirospringbootcourse/dao/BookDaoJdbcTemplateTest.java
+++ src/test/java/ua/com/dxrkness/devtirospringbootcourse/dao/BookDaoJdbcTemplateTest.java
@@ -8,26 +8,27 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ua.com.dxrkness.devtirospringbootcourse.domain.Author;
+import ua.com.dxrkness.devtirospringbootcourse.domain.Book;
 
 import static org.mockito.ArgumentMatchers.eq;
 
 @ExtendWith(MockitoExtension.class)
-public class AuthorDaoJdbcTemplateTest {
+public class BookDaoJdbcTemplateTest {
     @Mock
     private JdbcTemplate jdbcTemplate;
 
     @InjectMocks
-    private AuthorDaoJdbcTemplate authorDao;
+    private BookDaoJdbcTemplate bookDao;
 
     @Test
-    public void createAuthor_generatesValidSql() {
-        var author = new Author(1L, "Omar Hayam", 600);
+    public void createBook_generatesValidSql() {
+        var book = new Book("123-123", "Quatrians", new Author(1L, "Omar Hayam", 600));
 
-        authorDao.create(author);
+        bookDao.create(book);
 
         Mockito.verify(jdbcTemplate).update(
-                eq("INSERT INTO authors (id, name, age) VALUES (?, ?, ?)"),
-                eq(1L), eq("Omar Hayam"), eq(600)
+                eq("INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)"),
+                eq("123-123"), eq("Quatrians"), eq(1L)
         );
     }
 }
