bbbf3502bd1b68093bb13f8c66bf95248045beea
dxrknesss <imp0rt4ntduck@gmail.com>
2024-09-27 04:59:32 +0300

    feat: add new output for db query. refactor in to another class

--- src/main/java/ua/com/dxrkness/devtirospringbootcourse/DevtiroSpringBootCourseApplication.java
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/DevtiroSpringBootCourseApplication.java
@@ -1,32 +1,13 @@
 package ua.com.dxrkness.devtirospringbootcourse;
 
-import lombok.extern.java.Log;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
-@Log
-public class DevtiroSpringBootCourseApplication implements CommandLineRunner {
-
-    private final DataSource dataSource;
-
-    public DevtiroSpringBootCourseApplication(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+public class DevtiroSpringBootCourseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevtiroSpringBootCourseApplication.class, args);
     }
 
-
-    @Override
-    public void run(String... args) {
-        log.info("Datasource: " + dataSource);
-        final var jdbcTemplate = new JdbcTemplate(dataSource);
-        jdbcTemplate.execute("select 1");
-    }
 }
--- src/main/java/ua/com/dxrkness/devtirospringbootcourse/controller/HelloWorldController.java
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/controller/HelloWorldController.java
@@ -1,12 +1,38 @@
 package ua.com.dxrkness.devtirospringbootcourse.controller;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+
 @RestController
 public class HelloWorldController {
+    private final DataSource dataSource;
+    private JdbcTemplate template;
+
+    public HelloWorldController(DataSource dataSource) {
+        this.dataSource = dataSource;
+
+        template = new JdbcTemplate(dataSource);
+    }
+
     @GetMapping("/hello")
     public String helloWorld() {
+        var resultMap = new HashMap<String, List<String>>();
+        template.query("SELECT * FROM widgets",
+                (RowMapper<Object>) (rs, rowNum) ->
+                        resultMap.put(Integer.toString(rs.getRow()), List.of(
+                                rs.getString("id"),
+                                rs.getString("name"),
+                                rs.getString("purpose"))
+                        )
+        );
+
+        System.out.println(resultMap);
         return "Hello not quarkus!!";
     }
 }
