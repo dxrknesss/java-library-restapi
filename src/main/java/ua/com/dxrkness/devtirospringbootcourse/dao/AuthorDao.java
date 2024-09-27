8c50e1cf781571094edf02e2100a1e993bde833c
dxrknesss <imp0rt4ntduck@gmail.com>
2024-09-27 20:52:42 +0300

    feat: add create method for author dao + jdbcTemplate implementation

--- src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/AuthorDao.java
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/AuthorDao.java
@@ -1,4 +1,7 @@
 package ua.com.dxrkness.devtirospringbootcourse.dao;
 
+import ua.com.dxrkness.devtirospringbootcourse.domain.Author;
+
 public interface AuthorDao {
+    void create(Author author);
 }
--- src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/AuthorDaoJdbcTemplate.java
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/AuthorDaoJdbcTemplate.java
@@ -1,11 +1,20 @@
 package ua.com.dxrkness.devtirospringbootcourse.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import ua.com.dxrkness.devtirospringbootcourse.domain.Author;
 
 public class AuthorDaoJdbcTemplate implements AuthorDao {
     private final JdbcTemplate jdbcTemplate;
 
     public AuthorDaoJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void create(Author author) {
+        jdbcTemplate.update(
+                "INSERT INTO authors (id, name, age) VALUES (?, ?, ?)",
+                author.getId(), author.getName(), author.getAge()
+        );
     }
 }
