a0c8b48220df16b6cb8de210f7ddec5004b9792b
dxrknesss <imp0rt4ntduck@gmail.com>
2024-09-28 22:04:21 +0300

    feat: add implementation for createAuthor method on AuthorService

--- src/main/java/ua/com/dxrkness/devtirospringbootcourse/service/AuthorServiceImpl.java
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/service/AuthorServiceImpl.java
@@ -3,11 +3,18 @@ package ua.com.dxrkness.devtirospringbootcourse.service;
 
 import org.springframework.stereotype.Service;
 import ua.com.dxrkness.devtirospringbootcourse.domain.Author;
+import ua.com.dxrkness.devtirospringbootcourse.repository.AuthorRepository;
 
 @Service
 public class AuthorServiceImpl implements AuthorService {
+    private final AuthorRepository authorRepository;
+
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
     @Override
     public Author create(Author newAuthor) {
-        return null;
+        return authorRepository.save(newAuthor);
     }
 }
