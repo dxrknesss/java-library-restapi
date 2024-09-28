f2679db9e88383742c4ba8e4a0b5adb73d553623
dxrknesss <imp0rt4ntduck@gmail.com>
2024-09-28 11:42:10 +0300

    feat: add findOne method for AuthorDAO & jdbcTemplate implementation

--- src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/AuthorDao.java
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/AuthorDao.java
@@ -2,6 +2,9 @@ package ua.com.dxrkness.devtirospringbootcourse.dao;
 
 import ua.com.dxrkness.devtirospringbootcourse.domain.Author;
 
+import java.util.Optional;
+
 public interface AuthorDao {
     void create(Author author);
+    Optional<Author> findOne(Long id);
 }
--- src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/AuthorDaoJdbcTemplate.java
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/AuthorDaoJdbcTemplate.java
@@ -1,7 +1,12 @@
 package ua.com.dxrkness.devtirospringbootcourse.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import ua.com.dxrkness.devtirospringbootcourse.domain.Author;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
 
 public class AuthorDaoJdbcTemplate implements AuthorDao {
     private final JdbcTemplate jdbcTemplate;
@@ -16,5 +21,27 @@ public class AuthorDaoJdbcTemplate implements AuthorDao {
                 "INSERT INTO authors (id, name, age) VALUES (?, ?, ?)",
                 author.getId(), author.getName(), author.getAge()
         );
+    }
+
+    @Override
+    public Optional<Author> findOne(Long id) {
+        var results = jdbcTemplate.query(
+                "SELECT * FROM authors WHERE id = ? LIMIT 1",
+                new AuthorRowMapper(),
+                id
+        );
+
+        return results.stream().findFirst();
+    }
+
+    public static class AuthorRowMapper implements RowMapper<Author> {
+        @Override
+        public Author mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Author(
+                    rs.getLong("id"),
+                    rs.getString("name"),
+                    rs.getInt("age")
+            );
+        }
     }
 }
