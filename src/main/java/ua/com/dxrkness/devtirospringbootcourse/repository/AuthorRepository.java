6d07778db92140765e3582a90ba522abe5a0e6cd
dxrknesss <imp0rt4ntduck@gmail.com>
2024-09-28 19:51:40 +0300

    feat: add AuthorRepository custom queries

--- src/main/java/ua/com/dxrkness/devtirospringbootcourse/repository/AuthorRepository.java
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/repository/AuthorRepository.java
@@ -1,9 +1,15 @@
 package ua.com.dxrkness.devtirospringbootcourse.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ua.com.dxrkness.devtirospringbootcourse.domain.Author;
 
+import java.util.Set;
+
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, Long> {
+    Set<Author> findAllByAgeLessThan(int age);
+    @Query("SELECT aut FROM Author aut WHERE aut.age > ?1")
+    Set<Author> findAllWithAgeGreaterThan(int age);
 }
