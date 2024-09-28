47ce74f5c6b61fe993cfbcf390fe0b7e3df6f0fd
dxrknesss <imp0rt4ntduck@gmail.com>
2024-09-28 21:55:23 +0300

    feat: create AuthorService

---
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/service/AuthorService.java
@@ -0,0 +1,7 @@
+package ua.com.dxrkness.devtirospringbootcourse.service;
+
+import ua.com.dxrkness.devtirospringbootcourse.domain.Author;
+
+public interface AuthorService {
+    Author create(Author newAuthor);
+}
---
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/service/AuthorServiceImpl.java
@@ -0,0 +1,13 @@
+package ua.com.dxrkness.devtirospringbootcourse.service;
+
+
+import org.springframework.stereotype.Service;
+import ua.com.dxrkness.devtirospringbootcourse.domain.Author;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+    @Override
+    public Author create(Author newAuthor) {
+        return null;
+    }
+}
