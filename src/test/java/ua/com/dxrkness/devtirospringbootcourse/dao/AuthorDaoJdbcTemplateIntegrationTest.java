ecd37891ae1f8d4936201b8b3a5e5b10339faf4c
dxrknesss <imp0rt4ntduck@gmail.com>
2024-09-28 12:14:05 +0300

    test: add integration test for AuthorDaoJdbcTemplate

--- src/test/java/ua/com/dxrkness/devtirospringbootcourse/dao/AuthorDaoJdbcTemplateIntegrationTest.java
+++ src/test/java/ua/com/dxrkness/devtirospringbootcourse/dao/AuthorDaoJdbcTemplateIntegrationTest.java
@@ -1,15 +1,31 @@
 package ua.com.dxrkness.devtirospringbootcourse.dao;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class AuthorDaoJdbcTemplateIntegrationTest {
+
+    private AuthorDaoJdbcTemplate authorDao;
+
+    @Autowired
+    public AuthorDaoJdbcTemplateIntegrationTest(AuthorDaoJdbcTemplate authorDao) {
+        this.authorDao = authorDao;
+    }
+
     @Test
     public void author_canBeCreated_andThen_retrieved() {
+        var author = TestDataUtil.createTestAuthor();
 
+        authorDao.create(author);
+        var optionalAuthor = authorDao.findOne(author.getId());
+
+        Assertions.assertTrue(optionalAuthor.isPresent());
+        Assertions.assertEquals(optionalAuthor.get(), author);
     }
 }
