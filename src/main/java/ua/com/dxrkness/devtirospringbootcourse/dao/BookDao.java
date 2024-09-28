47937d52e367a6cec122b3e6bf3ecb3fa009f8ca
dxrknesss <imp0rt4ntduck@gmail.com>
2024-09-28 11:50:04 +0300

    feat: add findOne method for BookDAO & jdbcTemplate implementation

--- src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/BookDao.java
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/BookDao.java
@@ -2,6 +2,9 @@ package ua.com.dxrkness.devtirospringbootcourse.dao;
 
 import ua.com.dxrkness.devtirospringbootcourse.domain.Book;
 
+import java.util.Optional;
+
 public interface BookDao {
     void create(Book book);
+    Optional<Book> findOne(String isbn);
 }
--- src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/BookDaoJdbcTemplate.java
+++ src/main/java/ua/com/dxrkness/devtirospringbootcourse/dao/BookDaoJdbcTemplate.java
@@ -1,7 +1,13 @@
 package ua.com.dxrkness.devtirospringbootcourse.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import ua.com.dxrkness.devtirospringbootcourse.domain.Author;
 import ua.com.dxrkness.devtirospringbootcourse.domain.Book;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
 
 public class BookDaoJdbcTemplate implements BookDao {
     private final JdbcTemplate jdbcTemplate;
@@ -16,5 +22,27 @@ public class BookDaoJdbcTemplate implements BookDao {
                 "INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)",
                 book.getIsbn(), book.getTitle(), book.getAuthor().getId()
         );
+    }
+
+    @Override
+    public Optional<Book> findOne(String isbn) {
+        var results = jdbcTemplate.query(
+                "SELECT * FROM books WHERE isbn = ? LIMIT 1",
+                new BookRowMapper(),
+                isbn
+        );
+
+        return results.stream().findFirst();
+    }
+
+    public static class BookRowMapper implements RowMapper<Book> {
+        @Override
+        public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Book(
+                    rs.getString("isbn"),
+                    rs.getString("title"),
+                    new Author(rs.getLong("author_id"), null, null)
+            );
+        }
     }
 }
